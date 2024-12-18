9331f03a1e72fdf9b557eb917553ed33a4b68183
accesstbilq <accesstbilq@gmail.com>
2024-12-18 21:26:16 +0530

    changes commited

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import sys
 from dvclive import Live
 
 with Live(save_dvc_exp=True) as live:
-    epochs = 10
+    epochs =20
     live.log_param("epochs", epochs)
     for epoch in range(epochs):
         live.log_metric("train/accuracy", epoch + random.random())
