7c313f9fe7a33f45953797419581afd7392daec5
accesstbilq <accesstbilq@gmail.com>
2024-12-19 07:18:36 +0530

    zcommit

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import sys
 from dvclive import Live
 
 with Live(save_dvc_exp=True) as live:
-    epochs =20
+    epochs =30
     live.log_param("epochs", epochs)
     for epoch in range(epochs):
         live.log_metric("train/accuracy", epoch + random.random())
