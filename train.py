f830de591c2dbebdd172f1b196df921e8c493661
accesstbilq <accesstbilq@gmail.com>
2024-12-19 10:30:06 +0530

    Please push the code

--- train.py
+++ train.py
@@ -1,9 +1,11 @@
 import random
 import sys
+import yaml
 from dvclive import Live
 
 with Live(save_dvc_exp=True) as live:
-    epochs =50
+    trains_params = yaml.safe_load(open('params.yaml'))['train']
+    epochs = trains_params['epochs']
     live.log_param("epochs", epochs)
     for epoch in range(epochs):
         live.log_metric("train/accuracy", epoch + random.random())
